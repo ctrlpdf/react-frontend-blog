b5edec2515d1fe0ba5a0a854cd29698617a95ca8
ctrlpdf <ctrlpdf@gmail.com>
2023-07-10 23:25:14 +0900

    Added TagBox features

--- src/components/write/TagBox.js
+++ src/components/write/TagBox.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import styled from 'styled-components';
 // import palette from '../../lib/styles/palette';
 
@@ -18,25 +18,56 @@ const TagListBlock = styled.div`
   border: 3px solid violet;
 `;
 
-const TagItem = React.memo(({ tag }) => <Tag>#{tag}</Tag>);
+const TagItem = React.memo(({ tag, onRemove }) => (
+  <Tag onClick={() => onRemove(tag)}>#{tag}</Tag>
+));
 
-const TagList = React.memo(({ tags }) => (
+const TagList = React.memo(({ tags, onRemove }) => (
   <TagListBlock>
     {tags.map((tag) => (
-      <TagItem key={tag} tag={tag} />
+      <TagItem key={tag} tag={tag} onRemove={onRemove} />
     ))}
   </TagListBlock>
 ));
 
 const TagBox = () => {
+  const [input, setInput] = useState('');
+  const [localTags, setLocalTags] = useState([]);
+
+  const insertTag = useCallback(
+    (tag) => {
+      if (!tag) return;
+      if (localTags.includes(tag)) return;
+      setLocalTags([...localTags, tag]);
+    },
+    [localTags],
+  );
+  const onRemove = useCallback(
+    (tag) => {
+      setLocalTags(localTags.filter((t) => t !== tag));
+    },
+    [localTags],
+  );
+  const onChange = useCallback((e) => {
+    setInput(e.target.value);
+  }, []);
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      insertTag(input.trim());
+      setInput('');
+    },
+    [input, insertTag],
+  );
+
   return (
     <TagBoxBlock>
       <h4>Tags</h4>
-      <TagForm>
-        <input placeholder="Input tag" />
+      <TagForm onSubmit={onSubmit}>
+        <input placeholder="Input tag" value={input} onChange={onChange} />
         <button type="submit">Add</button>
       </TagForm>
-      <TagList tags={['Tag1', 'Tag2', 'Tag3']} />
+      <TagList tags={localTags} onRemove={onRemove} />
     </TagBoxBlock>
   );
 };
