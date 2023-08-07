ef61cbcdcf794180faf9dbbd123ac9ef5c4b3155
ctrlpdf <ctrlpdf@gmail.com>
2023-08-07 08:44:11 +0200

    Added post list

--- src/components/posts/PostList.js
+++ src/components/posts/PostList.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import Responsive from '../common/Responsive';
 import Button from '../common/Button';
 import SubInfo from '../common/SubInfo';
@@ -10,30 +11,46 @@ const WritePostButtonWrapper = styled.div``;
 
 const PostItemBlock = styled.div``;
 
-const PostItem = () => {
+const PostItem = ({ post }) => {
+  const { publishedDate, user, tags, title, body, _id } = post;
   return (
     <PostItemBlock>
-      <h2>title</h2>
-      <SubInfo username="username" publishedDate={new Date()} />
-      <Tags tags={['tag1', 'tag2', 'tag3']} />
-      <p>contents...</p>
+      <h2>
+        <Link to={`/@${user.username}/${_id}`}>{title}</Link>
+      </h2>
+      <SubInfo
+        username={user.username}
+        publishedDate={new Date(publishedDate)}
+      />
+      <Tags tags={tags} />
+      <p>{body}</p>
     </PostItemBlock>
   );
 };
 
-const PostList = () => {
+const PostList = ({ posts, loading, error, showWriteButton }) => {
+  // error!
+  if (error) {
+    return <PostListBlock>Error!</PostListBlock>;
+  }
+
   return (
     <PostListBlock>
       <WritePostButtonWrapper>
-        <Button cyan={true} to="/write">
-          New
-        </Button>
+        {showWriteButton && (
+          <Button cyan={true} to="/write">
+            New
+          </Button>
+        )}
       </WritePostButtonWrapper>
-      <div>
-        <PostItem />
-        <PostItem />
-        <PostItem />
-      </div>
+      {/* If it is not loading status and there is in an array of posts, then it will show */}
+      {!loading && posts && (
+        <div>
+          {posts.map((post) => (
+            <PostItem post={post} key={post._id} />
+          ))}
+        </div>
+      )}
     </PostListBlock>
   );
 };
